f0b3fa945b279051ce8ff8b5a3b11c9c95eee838
sluzerpp <bistboy.es@gmail.com>
2023-11-18 02:06:31 +0300

    feat: create product images page

--- Pages/assets/js/dragAndDrop.js
+++ Pages/assets/js/dragAndDrop.js
@@ -23,7 +23,6 @@ function onListDragOver(e, list) {
   }
 }
 
-
 function getDragAfterElement(container, y) {
   const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
 
@@ -36,4 +35,57 @@ function getDragAfterElement(container, y) {
       return closest
     }
   }, { offset: Number.NEGATIVE_INFINITY }).element
+}
+
+// function onGridListDragOver(e, list) {
+//   e.preventDefault();
+//   const draggable = list.querySelector('.dragging');
+//   const target = e.target.closest('.draggable:not(.dragging)');
+//   if (target && draggable) {
+//     const targetIndex = Array.from(list.children).indexOf(target);
+//     const draggedIndex = Array.from(list.children).indexOf(draggable);
+    
+//     if (targetIndex < draggedIndex) {
+//       list.insertBefore(draggable, target);
+//     } else {
+//       list.insertBefore(draggable, target.nextSibling);
+//     }
+//   }
+// }
+
+function onGridListDragOver(e, list) {
+  e.preventDefault();
+  const draggable = list.querySelector('.dragging');
+  const x = e.clientX || e.touches[0].clientX;
+  const y = e.clientY || e.touches[0].clientY;
+  
+  if (draggable) {
+    const pointElem = document.elementFromPoint(x, y);
+    
+    if (!pointElem) return;
+
+    const target = pointElem.closest('.draggable:not(.dragging)');
+
+    if (target) {
+      const targetIndex = Array.from(list.children).indexOf(target);
+      const draggedIndex = Array.from(list.children).indexOf(draggable);
+      
+      if (targetIndex < draggedIndex) {
+        list.insertBefore(draggable, target);
+      } else {
+        list.insertBefore(draggable, target.nextSibling);
+      }
+    }
+  }
+}
+
+function onImageDragStart(e, elem) {
+  const y = e.clientY || e.touches[0].clientY;
+  const x = e.clientX || e.touches[0].clientX;
+  const dragBtn = elem.querySelector('.drag-btn');
+  const box = dragBtn.getBoundingClientRect();
+  if (window.innerWidth < 1080 || (x >= box.left && x <= box.right && y >= box.top && y <= box.bottom)) {
+    e.stopPropagation();
+    elem.classList.add('dragging');
+  }
 }
