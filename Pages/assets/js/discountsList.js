b710cce2649a36bdf4b670df62d3d0df00dcf3c8
sluzerpp <bistboy.es@gmail.com>
2023-12-02 22:33:57 +0300

    fix: продукты основные сведения

---
+++ Pages/assets/js/discountsList.js
@@ -0,0 +1,68 @@
+function initDiscountList(list) {
+  list.style.opacity = 0;
+  const listWidth = list.getBoundingClientRect().width;
+  const items = list.querySelectorAll('li');
+  const arrow = list.querySelector('label');
+  const arrowWidth = arrow.getBoundingClientRect().width;
+  let lineWidth = arrowWidth;
+  let wasFalse = false;
+  const firstLineItems = [...items].filter((item) => {
+    if (wasFalse) return false;
+    const itemWidth = item.getBoundingClientRect().width;
+    if (lineWidth + itemWidth < listWidth) {
+      lineWidth += itemWidth;
+      return true;
+    }
+    wasFalse = true;
+    return false;
+  });
+
+  items.forEach((item) => item.remove());
+  arrow.remove();
+  list.append(...firstLineItems, arrow);
+
+  list.style.opacity = 1;
+
+  let isOpen = false;
+  
+  const createClickHandler = () => {
+    isOpen = !isOpen
+    if (isOpen) {
+      items.forEach((item) => item.remove());
+      arrow.remove();
+      arrow.classList.add('open');
+      list.append(...items, arrow);
+    } else {
+      items.forEach((item) => item.remove());
+      arrow.remove();
+      arrow.classList.remove('open');
+      list.append(...firstLineItems, arrow);
+    }
+  }
+
+  arrow.onclick = createClickHandler;
+}
+
+// Функция для обработки добавленных элементов списка
+function handleNewItems() {
+  initDiscountList();
+}
+
+let isInit = false;
+
+function initOnChange() {
+  if (!isInit) {
+    const discountLists = document.querySelectorAll('.product-varient__discounts .discounts-list');
+    discountLists.forEach((list) => {
+      initDiscountList(list);
+    })
+    isInit = true;
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const discountLists = document.querySelectorAll('.product__discounts .discounts-list');
+  discountLists.forEach((list) => {
+    initDiscountList(list);
+  })
+})
